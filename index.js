fe5980e4f017067b34860fb9c6c6406fec057fc9
Andy Liang <104800199+andy2131@users.noreply.github.com>
2022-06-29 06:43:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const reg = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+const symbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let output = document.getElementById("output")
+let length = document.getElementById("length") 
+let number = document.getElementById("numbers")
+let symbol = document.getElementById("symbols")
+
+function generate(array) {
+    let string=""
+    for (let i=0;i<length.value;i++) {
+        let index = Math.floor(Math.random()*(array.length))
+        string+=array[index] 
+    }
+    output.textContent = string
+}
+
+function main() {
+    if (number.checked==true && symbol.checked==true) {
+        generate(reg)
+    } 
+    else if (number.checked==false && symbol.checked==false) {
+        generate(letters)
+    } 
+    else if (number.checked==false) {
+        generate(numbers)
+    } 
+    else {
+        generate(symbols)
+    }
+}
+
+function copy() {
+    navigator.clipboard.writeText(output.textContent)
+    alert("Password has been copied to your clipboard!")
+}
+
+
+
